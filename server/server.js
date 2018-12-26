fb44628645decbca42b1be8fb8e8fdf00e365e97
Xiaoyun Yang <xiaoyuny.yxy@gmail.com>
2018-12-26 14:42:34 -0800

    Set up socket.io

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', socket => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected from server.');
+  });
+});
+
+server.listen(3000, () => {
+  console.log(`Server is up on ${port}`);
+});
